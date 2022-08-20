9c0811dcea50c1e08468d73273c1f5df0147f19d
admin <admin@eq19.com>
2022-08-20 14:20:11 +0700

    Create scripts.js

---
+++ docs/js/scripts.js
@@ -0,0 +1,50 @@
+window.include = function(url){
+	$('#₠Quantum').append('<script src="https://www.eq19.com//' + url + '"></script>');
+	return false;
+}
+
+/* core.JS
+ ========================================================*/
+include('slick/slick/slick.min.js');
+include('sequence/js/svginnerhtml.min.js');
+include('sequence/js/snap.svg-min.js');
+include('flowchart/raphael-min.js');
+include('route/d3.min.js');
+include('cards/widget.js');
+
+/* cookie.JS
+ ========================================================*/
+include('js/jquery.cookie.js');
+include('js/jquery-migrate-1.2.1.js');
+
+/* device JS
+ ========================================================*/
+include('js/device.min.js');
+include('js/tmstickup.js');
+
+/* Easing library
+ ========================================================
+include('js/jquery.ui.totop.js');
+include('js/jquery.easing.1.3.js');
+include('js/jquery.equalheights.js');
+
+
+/* Smooth scrolling
+ ========================================================*/
+include('/wheel/jquery.mousewheel.min.js');
+include('js/jquery.simplr.smoothscroll.min.js');
+
+/* Superfish menu
+ ========================================================*/
+include('js/superfish.js');
+include('js/jquery.unveil.js');
+include('js/jquery.mobilemenu.js');
+
+/* Custom script
+ ========================================================*/
+include('js/legacy.js');
+include('js/camera.js');
+include('js/flatdoc.js');
+include('js/isotope.min.js');
+include('js/owl.carousel.js');
+include('js/function.js');
