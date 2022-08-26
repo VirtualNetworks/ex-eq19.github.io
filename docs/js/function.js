3342c9b4acef270bbc4cd8a97c55241f0eb54e77
admin <admin@eq19.com>
2022-08-26 14:56:03 +0700

    Update function.js

--- docs/js/function.js
+++ docs/js/function.js
@@ -1,4 +1,11 @@
-var top_menu_height = 0;
+var vartop = top_menu_height = 0;
+
+// scroll animation 
+function scrollTo(selectors) {
+	if(!$(selectors).size()) return;
+	var selector_top = $(selectors).offset().top - top_menu_height;
+	$('html,body').animate({ scrollTop: selector_top }, 'slow');
+}
 
 jQuery(function($) {
 	$(window).load( function() {
@@ -64,11 +71,3 @@ jQuery(function($) {
 	});
 
 });
-
-// scroll animation 
-function scrollTo(selectors) {
-	if(!$(selectors).size()) return;
-	var selector_top = $(selectors).offset().top - top_menu_height;
-	$('html,body').animate({ scrollTop: selector_top }, 'slow');
-}
-
