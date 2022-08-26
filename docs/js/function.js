5abef2962510c4be148ea2e72c363a0b42f0061d
admin <admin@eq19.com>
2022-08-26 22:18:22 +0700

    Update function.js

--- docs/js/function.js
+++ docs/js/function.js
@@ -11,59 +11,56 @@ function scrollTo(selectors)
 }
 
 jQuery(function($) {
-		$(window).load( function() {
-			$('.external-link').unbind('click');	
-		});
-		
-        $(document).ready( function() {
+	$(window).load( function() {
+		$('.external-link').unbind('click');
+	});
 
-        top_menu_height = $('.templatemo-top-menu').height();
-        // scroll spy to auto active the nav item
-        $('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height + 10 });
+	$(document).ready( function() {
+
 		$('.external-link').unbind('click');
 
-        // scroll to top
-        $('#btn-back-to-top').click(function(e){
-            e.preventDefault();
-            scrollTo('#templatemo-top');
-        });
+		// scroll spy to auto active the nav item
+		top_menu_height = $('.templatemo-top-menu').height();
+		$('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height + 10 });
 
-        // scroll to specific id when click on menu
-        $('.templatemo-top-menu .navbar-nav a').click(function(e){
-            e.preventDefault(); 
-            var linkId = $(this).attr('href');
-            scrollTo(linkId);
-            if($('.navbar-toggle').is(":visible") == true){
-                $('.navbar-collapse').collapse('toggle');
-            }
-            $(this).blur();
-            return false;
-        });
-		/*
-		var dt = window.atob('IHwgRGVzaWduOiA8YSByZWw9Im5vZm9sbG93IiBocmVmPSJodHRwOi8vd3d3LnRlbXBsYXRlbW8uY29tL3RtLTM5NS11cmJhbmljIiB0YXJnZXQ9Il9wYXJlbnQiPlVyYmFuaWM8L2E+'); // decode the string
-		var div = document.getElementById('footer-line');
-		div.innerHTML = div.innerHTML + dt;
-		*/
-        // to stick navbar on top
+		// scroll to top
+		$('#btn-back-to-top').click(function(e){
+			e.preventDefault();
+			scrollTo('#templatemo-top');
+		});
+
+		// scroll to specific id when click on menu
+		$('.templatemo-top-menu .navbar-nav a').click(function(e){
+			e.preventDefault(); 
+			var linkId = $(this).attr('href');
+			scrollTo(linkId);
+			if($('.navbar-toggle').is(":visible") == true){
+				$('.navbar-collapse').collapse('toggle');
+			}
+			$(this).blur();
+			return false;
+		});
+
+		// to stick navbar on top
 			$.getScript("https://www.eq19.com/js/stickUp.min.js", function() {
 				$('.templatemo-top-menu ').stickUp();
 			});
 
-        // gallery category
-        $('.templatemo-gallery-category a').click(function(e){
-            e.preventDefault(); 
-            $(this).parent().children('a').removeClass('active');
-            $(this).addClass('active');
-            var linkClass = $(this).attr('href');
-            $('.gallery').each(function(){
-                if($(this).is(":visible") == true){
-                   $(this).hide();
-                };
-            });
-            $(linkClass).fadeIn();  
-        });
+		// gallery category
+		$('.templatemo-gallery-category a').click(function(e){
+			e.preventDefault(); 
+			$(this).parent().children('a').removeClass('active');
+			$(this).addClass('active');
+			var linkClass = $(this).attr('href');
+			$('.gallery').each(function(){
+				if($(this).is(":visible") == true){
+				   $(this).hide();
+				};
+			});
+			$(linkClass).fadeIn();  
+		});
 
-        //gallery light box setup
+		//gallery light box setup
 		$.getScript("https://www.eq19.com/colorbox/jquery.colorbox-min.js", function() {
 			$('a.colorbox').colorbox({
 				rel: function(){
@@ -72,5 +69,5 @@ jQuery(function($) {
 			});
 		});
 		
-    });
+	});
 });
