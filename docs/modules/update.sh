6f1f942078ede450ed0def3388fa361c6f5f4857
eq19 <admin@eq19.com>
2022-09-12 21:09:17 +0700

    Delete update.sh

--- docs/modules/update.sh
+++
@@ -1,5 +0,0 @@
-set sm_path=eQ19
-git submodule deinit -f --all
-git submodule update --init --recursive
-git submodule foreach 'git submodule set-branch --default -- ${sm_path}'
-git push --recurse-submodules=on-demand
