1edf138160542248dd0a5378b920400e313d5121
eq19 <admin@eq19.com>
2022-09-25 15:42:59 +0700

    excluded node modules

--- docs/.github/workflows/builders/docker/artifact.sh
+++ docs/.github/workflows/builders/docker/artifact.sh
@@ -21,6 +21,10 @@ pacman -Q
 echo -e "$hr\nBASH DETAIL\n$hr"
 pacman -Qii bash
 
+# file system
+echo -e "\n$hr\nFILE SYSTEM\n$hr"
+df -h
+
 # identity
 echo -e "\n$hrWHOAMI\n$hr"
 sudo chown -R $(id -u):$(id -g) $PWD
@@ -28,27 +32,34 @@ whoami
 pwd
 id
 
-echo -e "\n$hr\nHOME PROFILES\n$hr"
-ls -al $HOME
+# ls /
+echo -e "\n$hr\nALL REPOSITORY\n$hr"
+echo "/"
+ls -al /
 
+# root
 echo -e "$hr\nROOT PROFILES\n$hr"
+echo "/root"
 [[ "$(whoami)" == "root" ]] && ls -alL /root || sudo bash -c 'ls -alL /root'
 
+# root ssh
 echo -e "$hr\nSSH FILES\n$hr"
-echo $(whoami) /root/.ssh
+echo "/root/.ssh"
 [[ "$(whoami)" == "root" ]] && ls -alL /root/.ssh || sudo bash -c 'ls -alL /root/.ssh'
 
+# home
+echo -e "\n$hr\nHOME PROFILES\n$hr"
+echo $HOME
+ls -al $HOME
+
+# local bin
 echo -e "$hr\nBIN FILES\n$hr"
 echo $HOME/.local/bin
 ls -al $HOME/.local/bin
 
-echo -e "\n$hr\nFILE SYSTEM\n$hr"
-df -h
-
-echo -e "\n$hr\nALL REPOSITORY\n$hr"
-pwd
-ls -al /
-
+# workspace
 echo -e "\n$hr\nCURRENT REPOSITORY\n$hr"
 pwd
 ls -al .
+
+echo -e "\n$hr"
