262db9f8e530d7b25d2d1130b4c3c563a96617c8
eq19 <admin@eq19.com>
2022-09-27 15:40:32 +0700

    .gitignore

---
+++ docs/assets/_includes/templates/theme/script/init_environment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update packages database
+pacman -Syu --noconfirm
+
+# Installing git package
+pacman -S --noconfirm git
+
+# Installing ruby libraries
+pacman -S --noconfirm ruby2.7 ruby-bundler
+
+# Setting default ruby version
+cp /usr/bin/ruby-2.7 /usr/bin/ruby
+
+# debug
+ruby -v && bundle version
+
+# This is a temporary workaround
+# See https://github.com/actions/checkout/issues/766
+git config --global --add safe.directory "*"
