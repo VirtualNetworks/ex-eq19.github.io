ad0bf3dc601f2a65d6e4ea5a9e4d6c3eeb12ba40
eq19 <admin@eq19.com>
2022-10-02 21:53:40 +0700

    Update jekyll-datapage-generator.rb

--- docs/assets/_plugins/jekyll-datapage-generator.rb
+++ docs/assets/_plugins/jekyll-datapage-generator.rb
@@ -137,16 +137,17 @@ module Jekyll
 
       # data contains the specification of all the datasets for which we want
       # to generate individual pages (look at the README file for its documentation)
-      data = site.config['syntax_gen']
+      data = site.data['roots']
       if data
         data.each do |data_spec|
           index_files_for_this_data = false
           name_expr        = "'index_' + [100, 168, 618, record['pos'].chomp(';1;1;1').to_i].sum.to_s"
-          title_expr       = data_spec['title_expr']
-          title            = data_spec['title']
+          title_expr       = 'parser'
+          title            = 'parser'
           dir              = 'sitemap'
           template         = 'recipe'
           page_data_prefix = 'index_'
+          data             = 'roots'
           extension        = 'xml'
           name             = 'key'
           debug            = false
