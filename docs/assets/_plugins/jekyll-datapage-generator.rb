ab22456f7ac579208eab005098e86ab5b04d81b2
eq19 <admin@eq19.com>
2022-10-11 23:03:26 +0700

    require 'prime'

--- docs/assets/_plugins/jekyll-datapage-generator.rb
+++ docs/assets/_plugins/jekyll-datapage-generator.rb
@@ -123,9 +123,11 @@ module Jekyll
   end
 
   class JekyllDatapageGenerator < Generator
+    require 'prime'
     safe true
 
     # Finding Prime Numbers in Ruby
+    # https://github.com/ruby/prime
     def is_prime(num)
       (2..(num - 1)).each do |n|
         return false if num % n == 0
