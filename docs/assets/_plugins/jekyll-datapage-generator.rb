d4113f82aade9fe3b9508240c7f99b389caad205
eq19 <admin@eq19.com>
2022-10-17 21:41:04 +0700

    Update jekyll-datapage-generator.rb

--- docs/assets/_plugins/jekyll-datapage-generator.rb
+++ docs/assets/_plugins/jekyll-datapage-generator.rb
@@ -147,7 +147,7 @@ module Jekyll
         data.each do |row|
           set              = "index.prime?," * row['set'].to_i
           get              = ",index.prime?" * row['get'].to_i
-          name_expr        = "prefix + page_num.to_s"
+          name_expr        = "prefix + page_num.to_s + row['set']"
           title_expr       = "record['pos']"
           type             = row['type']
           up               = row['up']
